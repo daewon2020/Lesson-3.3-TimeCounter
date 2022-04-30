55fbdcdbbd3ca4c0887d5e123f5a234034ddce5d
Kostya Andreev <daewon@yandex.ru>
2022-04-30 14:15:46 +0300

    Initial Commit

---
+++ Lesson 3.3 TimeCounter/Lesson_3_3_TimeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_3_TimeCounterApp.swift
+//  Lesson 3.3 TimeCounter
+//
+//  Created by Константин Андреев on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_3_TimeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
